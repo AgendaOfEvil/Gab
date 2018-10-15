8f9fbfe28b5e6445c8581bc7abd9cb3de90229f3
dotnetnoobie <david.thiessen@gmail.com>
2018-10-15 18:10:53 +1100

    Added WPF example app for OAuth login via Gab API

---
+++ Gab.WPF/LoginDialog.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace Gab.WPF
+{
+    public partial class LoginDialog
+    {
+        string clientId = "< Your Client Id >";
+        string clientSecret = "< Your Client Secret >";
+        string callbackUrl = "< Your Callback URL >";
+
+        string scopes = "read engage-user engage-post write-post notifications";
+
+        public LoginDialog()
+        {
+            InitializeComponent();
+  
+            var startUri = new Uri($"https://api.gab.com/oauth/authorize?response_type=code&client_id={clientId}&redirect_uri={callbackUrl}&scope={scopes}");
+             
+            this.Browser.ScriptErrorsSuppressed = true;
+            this.Browser.Navigated += Browser_Navigated;
+            this.Browser.Navigate(startUri);
+        }
+
+        private async void Browser_Navigated(object sender, System.Windows.Forms.WebBrowserNavigatedEventArgs e)
+        { 
+            if (e.Url.PathAndQuery.Contains("code="))
+            { 
+                var code = e.Url.PathAndQuery.Split('=').Last();
+
+                var client = new HttpClient();
+
+                var parameters = new List<KeyValuePair<string, string>>();
+
+                parameters.Add(new KeyValuePair<string, string>("grant_type", "authorization_code"));
+                parameters.Add(new KeyValuePair<string, string>("code", code));
+                parameters.Add(new KeyValuePair<string, string>("client_id", clientId));
+                parameters.Add(new KeyValuePair<string, string>("client_secret", clientSecret));
+                parameters.Add(new KeyValuePair<string, string>("redirect_uri", callbackUrl));
+
+                var content = new FormUrlEncodedContent(parameters);
+
+                var url = "https://api.gab.com/oauth/token";
+
+                var tokenRes = await client.PostAsync(url, content);
+
+                var json = await tokenRes.Content.ReadAsStringAsync();
+
+                var token = JsonConvert.DeserializeObject<Token>(json);
+
+                ((MainWindow)Owner).Token = token;
+
+
+                this.Close();
+            }
+        } 
+    }
+}
---
+++ Gab.WPF/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace Gab.WPF
+{
+    public partial class MainWindow
+    {
+        public Token Token { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Instantiate the dialog box
+            var dlg = new LoginDialog();
+
+            // Configure the dialog box
+            dlg.Owner = this;
+            // dlg.DocumentMargin = this.documentTextBox.Margin;
+
+            dlg.Closed += Dlg_Closed;
+
+            // Open the dialog box modal mode 
+            dlg.ShowDialog();
+        }
+
+        private async void Dlg_Closed(object sender, EventArgs e)
+        {
+            var user = await UserDetails(Token);
+
+            Dispatcher.Invoke(() =>
+            {
+                Avatar.Source = new BitmapImage(new Uri(user.PictureUrl));
+                UserName.Text = $"@{user.Username}";
+                Name.Text = $"{user.Name}";
+            }, DispatcherPriority.Normal);
+        }
+
+        public async Task<User> UserDetails(Token token)
+        {
+            var url = "https://api.gab.com/v1.0/me/";
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
+            var json = await httpClient.GetStringAsync(url);
+
+            var serializer = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+
+            return JsonConvert.DeserializeObject<User>(json, serializer);
+        }
+    }
+}
